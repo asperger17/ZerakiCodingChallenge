ace1f9c485892f01281723ca9a086e49bfb8e5d3
Gabriel Owino <gabrielowino@Gabriels-MacBook-Pro.local>
2022-03-03 23:40:03 +0000

    First commit - spring initializr

---
+++ src/main/java/com/gabrielowino/zerakicodingchallenge/ZerakicodingchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielowino.zerakicodingchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerakicodingchallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerakicodingchallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabrielowino/zerakicodingchallenge/ZerakicodingchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielowino.zerakicodingchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerakicodingchallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
