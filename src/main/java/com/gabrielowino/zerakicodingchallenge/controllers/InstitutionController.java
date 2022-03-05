0bc633249a8e2f3895b3c948187aae8000c0279d
Gabriel Owino <gabrielowino@Gabriels-MacBook-Pro.local>
2022-03-05 15:15:54 +0000

    Add the institution rest API controller

---
+++ src/main/java/com/gabrielowino/zerakicodingchallenge/controllers/InstitutionController.java
@@ -0,0 +1,86 @@
+package com.gabrielowino.zerakicodingchallenge.controllers;
+
+import com.gabrielowino.zerakicodingchallenge.exceptionhandlers.exceptions.BadRequestException;
+import com.gabrielowino.zerakicodingchallenge.exceptionhandlers.exceptions.InstitutionNameAlreadyExistsException;
+import com.gabrielowino.zerakicodingchallenge.exceptionhandlers.exceptions.InstitutionNotFoundException;
+import com.gabrielowino.zerakicodingchallenge.exceptionhandlers.exceptions.InstitutionRegistrationNumberExistsException;
+import com.gabrielowino.zerakicodingchallenge.models.Institution;
+import com.gabrielowino.zerakicodingchallenge.repositories.InstitutionRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1")
+public class InstitutionController {
+
+    private final InstitutionRepository institutionRepository;
+
+    public InstitutionController(InstitutionRepository institutionRepository) {
+        this.institutionRepository = institutionRepository;
+    }
+
+    // List all the current institutions
+    @GetMapping("/institutions")
+    List<Institution> listAll(@RequestParam(value = "order", required = false) String order){
+        // List all the current institutions in default order
+        if (order == null) {
+            return institutionRepository.findAll();
+        // List all the current institutions in ascending order
+        } else if (order == "ascending"){
+            return institutionRepository.findAllByOrderByNameOfInstitutionAsc();
+        // List all the current institutions in descending order
+        } else if (order == "descending"){
+            return institutionRepository.findAllByOrderByNameOfInstitutionDesc();
+        } else {
+            throw new BadRequestException();
+        }
+    }
+
+    // Add a new institution
+    @PostMapping("/institutions")
+    Institution addNewInstitution(Institution newInstitution){
+        // Check if institutions with the same registration number or name exist
+        Optional<Institution> institution = institutionRepository.findById(newInstitution.getRegistrationNumber());
+        if (!institution.isEmpty()){
+            throw new InstitutionRegistrationNumberExistsException(newInstitution.getRegistrationNumber());
+        }
+        List<Institution> institutionList = institutionRepository.findByNameOfInstitution(
+                newInstitution.getNameOfInstitution());
+        if (!institutionList.isEmpty()){
+            throw new InstitutionNameAlreadyExistsException(newInstitution.getNameOfInstitution());
+        }
+        return institutionRepository.save(newInstitution);
+    }
+
+    @PutMapping("/institutions/{registrationNumber}")
+    Institution editNameOfInstitution(@PathVariable String registrationNumber, @RequestParam String nameOfInstitution) {
+        Optional<Institution> institutions = institutionRepository.findById(registrationNumber);
+        if (institutions.isPresent()) {
+            List<Institution> similarNamedInstitutions = institutionRepository.findByNameOfInstitution(nameOfInstitution);
+            if (similarNamedInstitutions.isEmpty()) {
+                Institution institution = institutionRepository.getById(registrationNumber);
+                institution.setNameOfInstitution(nameOfInstitution);
+                return institutionRepository.save(institution);
+            } else {
+                throw new InstitutionNameAlreadyExistsException(nameOfInstitution);
+            }
+        } else {
+            throw new InstitutionNotFoundException(registrationNumber);
+        }
+    }
+
+    @GetMapping("/institutions/{registrationNumber}")
+    Institution getInstitution(@PathVariable String registrationNumber){
+        return institutionRepository.findById(registrationNumber).orElseThrow(
+                () -> new InstitutionNotFoundException(registrationNumber)
+        );
+    }
+
+    @DeleteMapping("/institutions/{registrationNumber}")
+    void deleteInstitution(@PathVariable String registrationNumber){
+        institutionRepository.deleteById(registrationNumber);
+    }
+
+}
