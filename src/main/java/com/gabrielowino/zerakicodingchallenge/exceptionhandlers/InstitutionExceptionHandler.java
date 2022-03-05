7d066b772ebcc16d03f7d973d43f3b0b7066b01a
Gabriel Owino <gabrielowino@Gabriels-MacBook-Pro.local>
2022-03-05 17:16:43 +0000

    refactor institutions exceptions handler to make use the newly refactored exceptions

--- src/main/java/com/gabrielowino/zerakicodingchallenge/exceptionhandlers/InstitutionExceptionHandler.java
+++ src/main/java/com/gabrielowino/zerakicodingchallenge/exceptionhandlers/InstitutionExceptionHandler.java
@@ -1,12 +1,11 @@
 package com.gabrielowino.zerakicodingchallenge.exceptionhandlers;
 
-import com.gabrielowino.zerakicodingchallenge.exceptionhandlers.exceptions.InstitutionNotFoundException;
+import com.gabrielowino.zerakicodingchallenge.exceptionhandlers.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -15,7 +14,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class InstitutionExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ResponseBody
-    @ExceptionHandler(InstitutionNotFoundException.class)
+    @ExceptionHandler(ResourceNotFoundException.class)
     protected ResponseEntity<Object> handleExceptions(RuntimeException ex, WebRequest request){
         String bodyOfResponse = ex.getMessage();
         return handleExceptionInternal(ex, bodyOfResponse,
