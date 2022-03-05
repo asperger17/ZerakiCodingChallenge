9dc8779fe00273678ad4486c425d060f10fa47de
Gabriel Owino <gabrielowino@Gabriels-MacBook-Pro.local>
2022-03-05 17:17:23 +0000

    Create spring boot queries

--- src/main/java/com/gabrielowino/zerakicodingchallenge/repositories/CourseRepository.java
+++ src/main/java/com/gabrielowino/zerakicodingchallenge/repositories/CourseRepository.java
@@ -4,6 +4,18 @@ import com.gabrielowino.zerakicodingchallenge.models.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, String> {
+
+    List<Course> findAllByOrderByNameOfCourseAsc();
+
+    List<Course> findAllByOrderByNameOfCourseDesc();
+
+    List<Course> findCoursesByInstitutionOfferingCourse_RegistrationNumber(String registrationNumber);
+
+    List<Course> findCoursesByInstitutionOfferingCourse_RegistrationNumberAndNameOfCourse(
+            String registrationNumber, String courseName);
+
 }
