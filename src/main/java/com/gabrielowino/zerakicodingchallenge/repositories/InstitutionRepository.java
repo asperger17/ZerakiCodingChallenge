fea1c910a5b96a778a260ae8e261f1f075c9053f
Gabriel Owino <gabrielowino@Gabriels-MacBook-Pro.local>
2022-03-05 15:14:59 +0000

    Leverage Spring boot to create powerful queries

--- src/main/java/com/gabrielowino/zerakicodingchallenge/repositories/InstitutionRepository.java
+++ src/main/java/com/gabrielowino/zerakicodingchallenge/repositories/InstitutionRepository.java
@@ -4,6 +4,17 @@ import com.gabrielowino.zerakicodingchallenge.models.Institution;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InstitutionRepository extends JpaRepository<Institution, String> {
+
+    List<Institution> findByRegistrationNumberOrNameOfInstitution(
+            String registrationNumber, String nameOfInstitution);
+
+    List<Institution> findAllByOrderByNameOfInstitutionAsc();
+
+    List<Institution> findAllByOrderByNameOfInstitutionDesc();
+
+    List<Institution> findByNameOfInstitution(String nameOfInstitution);
 }
